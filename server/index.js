e71e43533b8113c83d9a766fc09965dd94f55612
Justin Cheng <justincheng2005@gmail.com>
2024-01-15 12:11:45 -0500

    connect database to server and installed dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE);
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${config.mongoUri}`)
+  })
 
 const app = express();
 
